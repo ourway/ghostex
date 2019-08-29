46aa0b063bba458fd455dcf25732db6916b86eac
Farsheed Ashouri <rodmena@me.com>
2019-08-29 16:05:44 +0430

    better docs

--- lib/ghostex.ex
+++ lib/ghostex.ex
@@ -6,12 +6,13 @@ defmodule Ghostex do
     to start using it.
 
     # Example
-
-    iex> admin_api_key = "d7f97d66265234665800be85170ae42:e4b59eaf9f4767e56f96753e0e6c91929dcb0284c2b101059bce954fd"
-    iex> api_url = "https://blog.example.org"
-    iex> {:ok, client} = Ghostex.Client.start_link [admin_api_key, api_url]
-    iex> resp = client |> Ghostex.Client.post("hi there", "Wow, __here__", ["test", "api"])
-    :ok
+    ```elixir
+      iex> admin_api_key = "d7f97d66265234665800be85170ae42:e4b59eaf9f4767e56f96753e0e6c91929dcb0284c2b101059bce954fd"
+      iex> api_url = "https://blog.example.org"
+      iex> {:ok, client} = Ghostex.Client.start_link [admin_api_key, api_url]
+      iex> resp = client |> Ghostex.Client.post("hi there", "Wow, __here__", ["test", "api"])
+      :ok
+  ```
 
   """
 end
